2cceedbd71011a50f7522fd83da056b191023583
Nicolás Quirós <59452965+NQuiros12@users.noreply.github.com>
2021-12-23 12:18:47 -0300

    Add files via upload

---
+++ Encriptar.py
@@ -0,0 +1,34 @@
+import string
+#Cargamos el mensaje.
+#fcenGuys = ["Sole","Fi","Joaco","AgusA","AgusB","Vicky","Juli"]
+mensaje = """Les queria agradecer por este gran año juntos, por bancarme siempre en todas mis ideas. 
+Por darme animos para cada uno de mis proyectos. Gracias por dejarme ser quien soy sin juzgarme nunca. Los amo"""
+mensaje = mensaje.lower()
+#Lo metemos dentro de una lista
+mensaje = list(mensaje)
+#Creamos una lista para el mensaje encriptado
+mensajeEncriptado = mensaje
+#lista de letras en ascii pero sin ñ
+abc = list(string.ascii_lowercase)
+#Creamos la funcion que encripta el mensaje, letra por letra.
+def VocaloConsonante(letra):
+    if(letra =="a" or
+        letra == "e" or letra == "i" or
+         letra == "o" or letra == "u"):
+         return abc.index(letra)
+    elif(letra == "ñ"):
+        return "ñ"
+    elif(letra == " " or letra == "." or letra == ","):
+        return " "
+    elif(letra == "\n"):
+        return "\n"
+    else:
+        return abc.index(letra)-1
+#Creamos un loop que corra por dentro de todas las letras del sistema.
+for i in range(len(mensaje)):
+    mensajeEncriptado[i] = VocaloConsonante(mensaje[i])
+
+print("El mensaje encriptado es")
+for numero in mensajeEncriptado:
+    print(numero,end = "//")
+print("\n")
